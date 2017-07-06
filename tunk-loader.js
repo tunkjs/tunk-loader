1f4d60bc35d8d2e6fe8ca40ea81cb2e03566baed
mac <522702986@qq.com>
2017-07-06 20:54:15 +0800

    dev

--- tunk-loader.js
+++ tunk-loader.js
@@ -1,9 +1,6 @@
 module.exports = function(source) {
-    const rpl = [];
-    source.replace(/@create[\w\W\n\r]*?class\s(\w*?)[\s\n\r]*?\{/g, function($1, $2){
-        rpl.push('\n' + $2 + '.__name__ = "' + $2 + '";');
-        return $1;
+    source = source.replace(/@create[\w\W\n\r]*?class\s+(\w*).*\{/g, function($1, $2){
+        return $1 + ' static __getName__(){return "' + $2 + '";}';
     });
-    console.log(rpl.join('\n'));
-    return source + rpl.join('\n');
+    return source;
 };
